37bddb26fc1b72dbbfe69a800d1eb513902b9ab1
Xisurthros <Jacob_Hobbs548@yahoo.com>
2022-09-13 16:51:02 -0500

    update main
    remove tinder.bio

--- tests/main.py
+++ tests/main.py
@@ -2,18 +2,16 @@ from tinder import Tinder
 
 def main():
 	function_dict = {'profile': tinder.profile, 'nearby': tinder.nearby,
-					'matches': tinder.matches, 'setBio': tinder.setBio,
-					'like': tinder.like, 'dislike': tinder.dislike}
+					'matches': tinder.matches, 'like': tinder.like,
+					'dislike': tinder.dislike}
 	while True:
 		user_input = input('Enter: ')
 		user_input.lower()
 		if user_input in function_dict:
 			print(function_dict[user_input]())
-		elif user_input == 'like':
-			tinder.like
 		else:
 			print('Invalid Input')
 
 if __name__ == '__main__':
-	tinder = Tinder('TOKEN')
+	tinder = Tinder('7cf84e76-f945-4988-85f7-788c57ca3463')
 	main()
