c91770660ce50762242d23df5ecbd0fbce709d21
Xisurthros <Jacob_Hobbs548@yahoo.com>
2022-09-13 16:45:49 -0500

    move main to test/

--- tests/main.py
+++ tests/main.py
@@ -1,12 +1,16 @@
 from tinder import Tinder
 
 def main():
-	function_dict = {'profile': tinder.profile, 'nearby': tinder.nearby}
+	function_dict = {'profile': tinder.profile, 'nearby': tinder.nearby,
+					'matches': tinder.matches, 'setBio': tinder.setBio,
+					'like': tinder.like, 'dislike': tinder.dislike}
 	while True:
 		user_input = input('Enter: ')
 		user_input.lower()
 		if user_input in function_dict:
 			print(function_dict[user_input]())
+		elif user_input == 'like':
+			tinder.like
 		else:
 			print('Invalid Input')
 
